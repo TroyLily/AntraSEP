5c4c838be55f0cea5922436319d4da1f86749c25
Troy <troy81908@gmail.com>
2022-04-23 13:56:23 -0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+1. What are the six combinations of access modifier keywords and what do they do? 
+    Public, Private, Protected, Internal, Protected Internal, Private Protected.
+2. What is the difference between the static, const, and readonly keywords when applied to a type member?
+    The first, const, is initialized during compile-time and the latter, readonly, initialized is by the latest 
+    run-time. 
+    The second difference is that readonly can only be initialized at the class-level.
+3. What does a constructor do?
+    constructor is used to create an object of the class and initialize class members
+4. Why is the partial keyword useful?
+    The partial keyword indicates that other parts of the class, struct, or interface can be defined 
+    in the namespace
+5. What is a tuple?
+    tuples feature provides concise syntax to group multiple data elements in a lightweight data structure
+6. What does the C# record keyword do?
+    Records are distinct from classes in that record types use value-based equality. Two variables of a 
+    record type are equal if the record type definitions are identical, and if for every field, the values 
+    in both records are equal. Two variables of a class type are equal if the objects referred to are the 
+    same class type and the variables refer to the same object. 
+7. What does overloading and overriding mean?
+    overloading is compile time polymorphism
+    overriding is rum-time polymorphism
+8. What is the difference between a field and a property?
+    a field is a variable of any type that is declared directly in the class while property is a member that 
+    provides a flexible mechanism to read, write or compute the value of a private field
+9. How do you make a method parameter optional?
+    by parameter keyword
+10. What is an interface and how is it different from abstract class?
+    An abstract class allows you to create functionality that subclasses can implement or override. 
+    An interface only allows you to define functionality, not implement it.
+11. What accessibility level are members of an interface?
+    
+12. True/False. Polymorphism allows derived classes to provide different implementations of the same method.
+    True
+13. True/False. The override keyword is used to indicate that a method in a derived class is providing 
+    its own implementation of a method.
+    
+14. True/False. The new keyword is used to indicate that a method in a derived class is providing its own 
+    implementation of a method.
+15. True/False. Abstract methods can be used in a normal (non-abstract) class. 
+16.True/False. Normal (non-abstract) methods can be used in an abstract class. 
+17. True/False. Derived classes can override methods that were virtual in the base class. 
+18. True/False. Derived classes can override methods that were abstract in the base class. 
+19. True/False. In a derived class, you can override a method that was neither virtual non abstract in the 
+    base class.
+20. True/False. A class that implements an interface does not have to provide an implementation for all 
+    of the members of the interface.
+21. True/False. A class that implements an interface is allowed to have other members that aren’t defined 
+    in the interface.
+22. True/False. A class can have more than one base class.
+23. True/False. A class can implement more than one interface.
+*/
