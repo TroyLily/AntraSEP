e1c96b237791447875d9759f25d9dd6eded9fcc8
Troy <troy81908@gmail.com>
2022-04-25 01:31:37 -0700

    Add files via upload

---
+++ ConsoleApp3/Generics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public static class Generics
+    {
+        public static bool AreEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+}
